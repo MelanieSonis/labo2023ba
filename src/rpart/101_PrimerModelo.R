55437895f1b8959dc4980c74306f51fa6f8df561
MelanieSonis <melaniesonis123mel@gmail.com>
2023-08-19 15:26:34 +0000

    101_004

--- src/rpart/101_PrimerModelo.R
+++ src/rpart/101_PrimerModelo.R
@@ -22,7 +22,7 @@ modelo <- rpart(
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
         cp = -0.3, # esto significa no limitar la complejidad de los splits
-        minsplit = 0, # minima cantidad de registros para que se haga el split
+        minsplit = 300, # minima cantidad de registros para que se haga el split
         minbucket = 10, # tamaño minimo de una hoja
         maxdepth = 20
 ) # profundidad maxima del arbol
@@ -60,6 +60,6 @@ dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_003.csv",
+        file = "./exp/KA2001/K101_004.csv",
         sep = ","
 )
