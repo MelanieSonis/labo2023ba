f17642e8296df9c7350b74fd34609d04b48a030c
MelanieSonis <melaniesonis123mel@gmail.com>
2023-09-18 21:31:03 +0000

    651_005

--- src/workflow-inicial/651_HT_lightgbm_005.r
+++ src/workflow-inicial/651_HT_lightgbm_005.r
@@ -79,7 +79,7 @@ PARAM$bo_lgb <- makeParamSet(
   makeIntegerParam("lambda_l2", lower = 0.0, upper = 1000.0),
   makeIntegerParam("bagging_freq", lower = 0L, upper = 6L),
   makeIntegerParam("min_gain_to_split", lower = 0L, upper = 5L),
-  makeIntegerParam("min_data_in_bin", lower = 1L, upper = 5L)
+  makeIntegerParam("early_stopping_round", lower = 0L, upper = 50L)
 )
 
 
