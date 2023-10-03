3e7dbce43fb5d5ba058e80b509502a79c28cfbfb
MelanieSonis <melaniesonis123mel@gmail.com>
2023-10-03 11:24:28 +0000

    finals

--- src/workflow-inicial/601_RUN_correr_workflow_005.r
+++ src/workflow-inicial/601_RUN_correr_workflow_005.r
@@ -13,7 +13,7 @@ options(error = function() {
 source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_002.r")
 source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_002.r")
 source("~/labo2023ba/src/workflow-inicial/631_FE_historia_002.r")
-source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_002.r")
+source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_004.r")
 
 # ultimos pasos, muy lentos
 source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_026.r")
